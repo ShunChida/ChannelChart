bb7a07cdd98dc855dff614be530e77b0bc1847c8
shun.chida <chida-shun-hz@ynu.jp>
2022-11-01 08:50:58 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
             <h1>ChannelChartとは？</h1>
             <h4>あなたのYouTubeアカウントで登録しているチャンネルを自由にリスト化して、<br>
             リスト内のチャンネルのコンテンツ配信状況を確認できます。</h4>
+            {!! link_to_route('signup.get', 'ユーザー登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
